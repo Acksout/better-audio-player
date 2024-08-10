5c7b7702044866fd1fcfc2b52a807d103d5f1187
Rakshit Bhardwaj <157364563+Acksout@users.noreply.github.com>
2024-08-10 18:01:09 +0530

    change title

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import React from "react";
 export default function Home() {
     return (
         <div>
-            <h1 className="m-2 text-center text-xl">100 Audio Player</h1>
+            <h1 className="m-2 text-center text-xl">808 Vault</h1>
             <div className="flex items-center text-sm">
                 <p className="pl-[653px]">Date Modified</p>
                 <p className="pl-[133px]">Size</p>
